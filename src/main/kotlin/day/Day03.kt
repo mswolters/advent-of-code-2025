328f0d933a9a62b90b46120f4cb097ffcb1068c5
Maaike Wolters <maaike@drbreakalot.nl>
2025-12-03 22:13:45 +0100

    Day 3 part 1

--- src/main/kotlin/day/Day03.kt
+++ src/main/kotlin/day/Day03.kt
@@ -2,7 +2,20 @@ package day
 
 object Day03 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        return input.map { line -> line.toCharArray().map { c -> c.digitToInt() } }
+            .sumOf { findMaxJoltage(it) }
+            .asSuccess()
+    }
+
+    private fun findMaxJoltage(bank: List<Int>): Int {
+        var max = bank.max()
+        var indexMax = bank.indexOf(max)
+        if (indexMax == bank.size - 1) {
+           max = bank.filter { it != max }.max()
+           indexMax = bank.indexOf(max)
+        }
+        val secondMax = bank.drop(indexMax + 1).max()
+        return max * 10 + secondMax
     }
 
     override fun part2(input: List<String>): Result {
@@ -11,9 +24,14 @@ object Day03 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            357,
             0,
-            0,
-            """"""
+            """
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111
+            """.trimIndent()
                 .lines()
         )
     }
