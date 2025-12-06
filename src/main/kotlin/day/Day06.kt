cf2a87057aaf3b8b4eed26cff241286e6e11abc4
Maaike Wolters <maaike@drbreakalot.nl>
2025-12-06 18:09:33 +0100

    Day 6 part 1

--- src/main/kotlin/day/Day06.kt
+++ src/main/kotlin/day/Day06.kt
@@ -1,8 +1,28 @@
 package day
 
+import asLongs
+import multiplied
+import toRectangle
+
 object Day06 : Day {
+    val anySpaces = Regex("""\s+""")
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        return input.map { it.trim().split(anySpaces) }
+            .toRectangle()
+            .columns()
+            .sumOf { calculateProblem(it) }
+            .asSuccess()
+    }
+
+    private fun calculateProblem(problem: List<String>): Long {
+        val op = problem.last()
+        val numbers = problem.dropLast(1).asLongs()
+
+        return when (op) {
+            "+" -> numbers.sum()
+            "*" -> numbers.multiplied()
+            else -> throw IllegalArgumentException("unknown op $op")
+        }
     }
 
     override fun part2(input: List<String>): Result {
@@ -11,9 +31,14 @@ object Day06 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            4277556,
             0,
-            0,
-            """"""
+            """
+                123 328  51 64 
+                 45 64  387 23 
+                  6 98  215 314
+                *   +   *   + 
+            """.trimIndent()
                 .lines()
         )
     }
