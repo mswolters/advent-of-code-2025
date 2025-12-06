d100e284612ce8299e5b5f3cc49fe39cfc825c21
Maaike Wolters <maaike@drbreakalot.nl>
2025-12-06 16:55:29 +0100

    Day 5 part 1

--- src/main/kotlin/day/Day05.kt
+++ src/main/kotlin/day/Day05.kt
@@ -1,8 +1,15 @@
 package day
 
+import asLongs
+import split
+
 object Day05 : Day {
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        val (rangeLines, idLines) = input.split("")
+        val ranges = rangeLines.map { it.split("-") }
+            .map { (first, last) -> LongRange(first.toLong(), last.toLong()) }
+
+        return idLines.asLongs().count { id -> ranges.any { id in it } }.asSuccess()
     }
 
     override fun part2(input: List<String>): Result {
@@ -11,9 +18,21 @@ object Day05 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            3,
             0,
-            0,
-            """"""
+            """
+                3-5
+                10-14
+                16-20
+                12-18
+
+                1
+                5
+                8
+                11
+                17
+                32
+            """.trimIndent()
                 .lines()
         )
     }
