a1bbf9f6bf68b90d86785df45ddc2c6347e1f7e0
Maaike Wolters <maaike@drbreakalot.nl>
2025-12-16 22:25:37 +0100

    Day 9 part 1

--- src/main/kotlin/day/Day09.kt
+++ src/main/kotlin/day/Day09.kt
@@ -1,8 +1,19 @@
 package day
 
+import allPairs
+import asInts
+import kotlin.math.abs
+
 object Day09 : Day {
+
+    data class Tile(val x: Int, val y: Int)
+
     override fun part1(input: List<String>): Result {
-        return NotImplemented
+        val tiles = input.map { it.split(",").asInts() }.map { (x, y) -> Tile(x, y) }
+
+        return tiles.allPairs()
+            .maxOf { (first, last) -> (abs(first.x - last.x) + 1) * (abs(first.y - last.y) + 1).toLong() }
+            .asSuccess()
     }
 
     override fun part2(input: List<String>): Result {
@@ -11,9 +22,18 @@ object Day09 : Day {
 
     override fun testData(): Day.TestData {
         return Day.TestData(
+            50,
             0,
-            0,
-            """"""
+            """
+                7,1
+                11,1
+                11,7
+                9,7
+                9,5
+                2,5
+                2,3
+                7,3
+            """.trimIndent()
                 .lines()
         )
     }
